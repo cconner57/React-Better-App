86902e98ea46e37e7d27a5a8b7036697f467d4c4
Chris Conner <chris.conner57@gmail.com>
2021-08-31 14:43:41 -0700

    Add dynamic routing

--- src/navigation/Navigation.tsx
+++ src/navigation/Navigation.tsx
@@ -6,26 +6,28 @@ import {
 	Switch,
 } from 'react-router-dom';
 
-import Home from '../screens/Home';
-import Details from '../screens/Details';
-import EditFeedback from '../screens/EditFeedback';
-import NewFeedback from '../screens/NewFeedback';
-import Roadmap from '../screens/Roadmap';
-import Suggestions from '../screens/Suggestions';
+const Home = lazy(() => import('../screens/Home'));
+const Details = lazy(() => import('../screens/Details'));
+const EditFeedback = lazy(() => import('../screens/EditFeedback'));
+const NewFeedback = lazy(() => import('../screens/NewFeedback'));
+const Roadmap = lazy(() => import('../screens/Roadmap'));
+const Suggestions = lazy(() => import('../screens/Suggestions'));
 
 const Navigation = () => {
 	return (
-		<Router>
-			<Switch>
-				<Route path='/details/:id' exact component={Details} />
-				<Route path='/edit' exact component={EditFeedback} />
-				<Route path='/new' exact component={NewFeedback} />
-				<Route path='/roadmap' exact component={Roadmap} />
-				<Route path='/suggestions' exact component={Suggestions} />
-				<Route path='/' exact component={Home} />
-				<Redirect to='/' />
-			</Switch>
-		</Router>
+		<Suspense fallback={<p>Loading...</p>}>
+			<Router>
+				<Switch>
+					<Route path='/details/:id' exact component={Details} />
+					<Route path='/edit' exact component={EditFeedback} />
+					<Route path='/new' exact component={NewFeedback} />
+					<Route path='/roadmap' exact component={Roadmap} />
+					<Route path='/suggestions' exact component={Suggestions} />
+					<Route path='/' exact component={Home} />
+					<Redirect to='/' />
+				</Switch>
+			</Router>
+		</Suspense>
 	);
 };
 
