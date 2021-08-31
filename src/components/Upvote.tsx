6238df8646656d97d346324a114b98060c11af55
Chris Conner <chris.conner57@gmail.com>
2021-08-31 14:43:58 -0700

    Fix upvote bug

--- src/components/Upvote.tsx
+++ src/components/Upvote.tsx
@@ -1,11 +1,25 @@
-// import UpArrow from '../assets/icon-arrow-up.svg';
+import { useState } from 'react';
 import { ReactComponent as UpArrow } from '../assets/icon-arrow-up.svg';
 
 const Upvote = ({ votes, direction }: { votes: number; direction: string }) => {
+	const [count, setCount] = useState(votes);
+	const [clicked, setClicked] = useState(false);
+
+	const handleClick = () => {
+		if (!clicked) {
+			setCount(count + 1);
+			setClicked(true);
+		}
+	};
+
 	return (
-		<div className={`Upvote ${direction === 'row' ? 'Row' : 'Column'}`}>
+		<div
+			className={`Upvote ${clicked && 'Clicked'} ${
+				direction === 'row' ? 'Row' : 'Column'
+			}`}
+			onClick={handleClick}>
 			<UpArrow />
-			<p>{votes}</p>
+			<p>{count}</p>
 		</div>
 	);
 };
